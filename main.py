12fc45487ccd8c619591a46a97c89baf31cf7d17
Alina Lavrik <alina_voro@mail.ru>
2023-01-11 23:42:42 +0300

    Add seminar task_10

---
+++ main.py
@@ -0,0 +1,7 @@
+import requests
+
+res = requests.get('https://www.cbr-xml-daily.ru/daily_json.js').json()
+# for i in res.values():
+#     print(i)
+print(res['Valute']['USD']['Name'], res['Valute']['USD']['Value'])
+print(res['Valute']['EUR']['Name'], res['Valute']['EUR']['Value'])
